9103683b59fb58aa697c3d54bbc54f009d6e8a47
Lee Thoong Ching <pakkua80@yahoo.com>
2025-11-10 11:38:17 +0800

    Update docker-script.sh
    line 7, and line 10
    
    --dryrun true replaced with -x
    
    Getting it up to date with new argument

--- src/docker-script.sh
+++ src/docker-script.sh
@@ -4,10 +4,10 @@ RETENTION_DAYS="${RETENTION_IN_DAYS:-7}"
 if [[ "${DRY_RUN}" = "true" ]]; then
     echo "== DRY RUN EXECUTION"
     if [[ -z "${WIKI}" ]]; then
-        ./backup-devops.sh -p $DEVOPS_PAT -o $DEVOPS_ORG_URL -d /data -r $RETENTION_DAYS --dryrun true
+        ./backup-devops.sh -p $DEVOPS_PAT -o $DEVOPS_ORG_URL -d /data -r $RETENTION_DAYS -x
     else
         echo "== INCLUDE WIKI"
-        ./backup-devops.sh -p $DEVOPS_PAT -o $DEVOPS_ORG_URL -d /data -r $RETENTION_DAYS --dryrun true -w
+        ./backup-devops.sh -p $DEVOPS_PAT -o $DEVOPS_ORG_URL -d /data -r $RETENTION_DAYS -x -w
     fi
 else
     if [[ -z "${WIKI}" ]]; then
